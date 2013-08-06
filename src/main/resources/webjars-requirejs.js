94ff6231a85d5f310d77474282b30509c40480f6
James Ward <james@jamesward.org>
2013-08-06 07:20:37 -0600

    correct webjar path

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,7 +1,7 @@
 requirejs.config({
     map: {
         '*': {
-            'css': 'require-css/css' // or whatever the path to require-css is
+            'css': 'webjars!css'
         }
     }
 });
